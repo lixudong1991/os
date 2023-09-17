534da759ec4c134e04cab6ccd6e3bdcbcbfe92b2
lixudong <lixudong0325@163.com>
2023-09-17 18:20:44 +0800

    添加键盘鼠标驱动

--- boot.asm
+++ boot.asm
@@ -1,7 +1,6 @@
 
 global setgdtr,setldtr,settr,cs_data,ds_data,ss_data,fs_data,gs_data,cpuidcall,rdmsrcall,wrmsrcall,wrmsr_fence,rdmsr_fence,setds,setgs,setfs,esp_data,cr3_data,flags_data,setBit,resetBit,testBit,allocatePhy4kPage,freePhy4kPage,sysInLong,sysOutLong,callTss,setidtr,cli_s,sti_s,invlpg_s,intcall,resetcr3,rtc_8259a_enable,interrupt8259a_disable
-global _monitor,_mwait,cr0_data,set_cr0data,cr4_data,set_cr4data,pre_mtrr_change,post_mtrr_change,spinlock,unlock,sysInChar,sysOutChar,ps2Controllerinit,wait60PortToread,wait60_64PortTowirte
-extern bootparam
+global _monitor,_mwait,cr0_data,set_cr0data,cr4_data,set_cr4data,pre_mtrr_change,post_mtrr_change,spinlock,unlock,sysInChar,sysOutChar
 pageStatusOffset equ 28
 IA32_MTRR_DEF_TYPE_MSR equ 0x2FF
 setgdtr:
